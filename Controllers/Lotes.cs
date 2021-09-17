e9417425c15944ea423cf513d7ceaa6b000eedfc
1711 MXTI <1711 MXTI@1711_MXTI>
2021-09-17 17:03:09 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/Lotes.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BovitratoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LotesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string connectionString = "Data Source=localhost;Initial Catalog=Bovitrato;Integrated Security=True";
+
+            string sql =
+                "select TOP 10 [LoteNumero] as codigo " +
+                ", LoteSexo as sexo " +
+                ", [LoteDataEntrada] as dataEntrada " +
+                ", [LoteDataSaida] as dataSaida " +
+                ", [LotePesoCarcaca] as pesoCarcaca " +
+                ", [LotePesoBrutoCarcaca] as pesoBrutoCarcaca " +
+                ", [LoteManejoId] " +
+                ", [LoteGMD] as gmd " +
+                ", [LoteDataAbate] as dataAbate " +
+                ", [LoteDiasCochoEsperado] as dcEsperado " +
+                ", [LoteRendEsperado] as rendEsperado " +
+                "from Lote " +
+                "FOR JSON PATH";
+
+            string retorno = "";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                try
+                {
+                    conn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            retorno += dr[0].ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return retorno;
+        }
+    }
+}
+
+
---
+++ Lotes.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BovitratoApi
+{
+    public class Lotes
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
